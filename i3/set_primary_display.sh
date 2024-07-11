a8e8abe3425524d1a45bc51fddf1323182757593
Ruslan Amirov <ruslan.amirov@evocargo.com>
2024-07-11 14:55:36 +0300

    handle external monitors

---
+++ i3/set_primary_display.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Get the connection status of HDMI monitor
+HDMI_CONNECTED=$(xrandr | grep "HDMI-1 connected")
+echo "Script executed!" >> ~/script_log.txt
+if [ -n "$HDMI_CONNECTED" ]; then
+    # HDMI monitor is connected
+    xrandr --output HDMI-1 --primary --auto --above eDP-1
+    xrandr --output eDP-1 --off
+else
+    # HDMI monitor is not connected, use laptop's built-in display
+    xrandr --output eDP-1 --primary --auto
+    xrandr --output HDMI-1 --off
+fi
+
