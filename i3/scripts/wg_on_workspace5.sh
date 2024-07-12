f0a93af2c9ce0df9010c097991f82f8aea46541e
Ruslan Amirov <ruslan.amirov@evocargo.com>
2024-07-13 01:33:55 +0300

    Keep Personal WG only on Workspace 5

--- i3/scripts/wg_on_workspace5.sh
+++ i3/scripts/wg_on_workspace5.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-i3-msg -t subscribe -m '[ "workspace" ]' | while read -r line; do
+function wg_handler(){
+    tmux new-session -s wg -d
+    i3-msg -t subscribe -m '[ "workspace" ]' | while read -r line; do
     workspace_name=$(echo "$line" | jq -r '.current.name')
     if [ "$workspace_name" = "5" ]; then
-        echo "$USER_PASSWORD" | sudo -S wg-quick up blanc
+        command="echo $(cat ~/.password) | sudo -S wg-quick up blanc"
     else
-        echo "$USER_PASSWORD" | sudo -S wg-quick down blanc
+        command="echo $(cat ~/.password) | sudo -S wg-quick down blanc"
     fi
+    tmux send -t wg.0 "$command" ENTER
 done
+}
 
+wg_handler
--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -22,9 +22,7 @@ vim.opt.mouse = ''
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.opt.breakindent = true
