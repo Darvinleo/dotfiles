157b63d09ba044b9154d5a9bb0f36afb8e362ffd
Ruslan Amirov <ruslan.amirov@evocargo.com>
2025-03-17 11:48:27 +0300

    add tmux

--- nvim/lua/plugins/kitty-scrollback.lua
+++ nvim/lua/plugins/kitty-scrollback.lua
@@ -4,8 +4,8 @@ return {
   lazy = true,
   cmd = { "KittyScrollbackGenerateKittens", "KittyScrollbackCheckHealth" },
   event = { "User KittyScrollbackLaunch" },
-  -- version = '*', -- latest stable version, may have breaking changes if major version changed
-  -- version = '^5.0.0', -- pin major version, include fixes and features that do not have breaking changes
+  version = "*", -- latest stable version, may have breaking changes if major version changed
+  -- version = "6.1.2", -- pin major version, include fixes and features that do not have breaking changes
   config = function()
     require("kitty-scrollback").setup()
   end,
