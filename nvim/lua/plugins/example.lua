dda554a6762c71f5c53444a34c3988dc98711ed2
Ruslan Amirov <ruslan.amirov@evocargo.com>
2025-04-01 10:20:58 +0300

    adjust for pc

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -63,20 +63,6 @@ return {
     },
   },
 
-  -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        -- pyright will be automatically installed with mason and loaded with lspconfig
-        pyright = {},
-        bashls = {},
-      },
-    },
-  },
-
   -- add tsserver and setup with typescript.nvim instead of lspconfig
   {
     "neovim/nvim-lspconfig",
